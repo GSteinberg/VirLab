32371759e9d39c8ecefe7036127b8e7052cd3be0
Gabriel Steinberg <gsteinb1@binghamton.edu>
2018-03-16 16:30:32 -0400

    function skeletons

--- fasta_parser.py
+++ fasta_parser.py
@@ -1,1 +1,19 @@
 #FASTA Parser
+import biopython as bp
+
+# takes in FASTA file or FASTA file segment as input
+
+#Start at certain char and end at certain char to get one genome for testing in beginning
+
+def fasta_parser(file) {
+    faa = read(file)
+
+    #use biopython str(faa) function to ocnvert to string
+    
+    return bp.str(faa)
+}
+
+#
+
+# output: single string or maybe array(?) of strings?
+
--- k_mer_creator.py
+++ k_mer_creator.py
@@ -1,1 +1,12 @@
 #K-mer creator
+
+import fasta_parser as fp
+
+#from main: string = fp.somefunction(file)
+
+def analyze(string, int k)
+    # input is string
+
+    # BODY: iterate window of k-chars and catalog them in dictionary :
+    
+    # output is dictionary of k-mers with their frequency (ie. k-mer = key, freq = value)
