aade46f7845d018eda72567072ed271883ce87b2
Gabriel Steinberg <gppsteinberg@gmail.com>
2019-01-15 15:00:32 -0500

    Made true testing set scalable

--- k_mer_creator.py
+++ k_mer_creator.py
@@ -4,6 +4,7 @@ import csv
 import kruskal_wallis as kw
 import visualize as viz
 from genome import Genome
+import random
 
 def toZero( list, kmer ):
 	list[kmer] = 0
@@ -17,9 +18,16 @@ def analyze_range( k_min, k_max, rootdir, dataset1, dataset2 ):
 	# replace blanks with zeros
 	# checks if any k-mers are not present in all other genomic kmer dictionaries
 	# if not found, adds them with a value of 0 so all genomes have the same kmer features
+	# this takes over 1/3 of our computation time
 	for g in genomes:
 		[toZero(g.kmers, kmer) for g2 in genomes for kmer in sorted(g2.kmers.keys()) if kmer not in g.kmers.keys()]
 	print("zeros added")
+	
+	# split into test and train sets
+	random.shuffle(genomes)
+	pivot = int(len(genomes)*0.7)
+	train_data = genomes[:pivot]
+	test_data = genomes[pivot:]
 	
 	#make csv file with each row having key and value
 	filename = "%s-%smers in %s and %s.csv" % (k_min, k_max, dataset1.strip("\\"), dataset2.strip("\\"))
@@ -27,19 +35,18 @@ def analyze_range( k_min, k_max, rootdir, dataset1, dataset2 ):
 		# Instantiate labels
 		fieldnames = []
 		# convert to list comprehension
-		for g in genomes:
+		for g in train_data:
 			for key in sorted(g.kmers.keys()):
 				if key not in fieldnames:
 					fieldnames.append(key)
 					
 		writer = csv.DictWriter(csv_file, fieldnames=sorted(fieldnames))
 		writer.writeheader()
-		for g in genomes:
+		for g in train_data:
 			writer.writerow(g.kmers)
 		csv_file.flush()
 	
 	flipped_list = zip(*csv.reader(open(filename, "r")))
-	
 	with open(filename, "w", newline="") as flipped_csv:
 		csv_writer = csv.writer(flipped_csv)
 		vector_list = []
@@ -49,13 +56,14 @@ def analyze_range( k_min, k_max, rootdir, dataset1, dataset2 ):
 		csv_writer.writerow(vector_list)
 		csv_writer.writerows(flipped_list)
 		
+	# takes about 1/3 of computation time
 	retlist = kw.test( filename, dataset1, dataset2 )
 		
 	# Accepting significant K-mers and making final csv
-	with open("significant_k_mers.csv", 'w', newline="") as csv_file:
+	with open("training_sig_k_mers.csv", 'w', newline="") as csv_file:
 		writer = csv.DictWriter(csv_file, fieldnames=retlist, extrasaction='ignore')
 		writer.writeheader()
-		for g in genomes:
+		for g in train_data:
 			if g.vector == dataset1[1:]:
 				g.kmers["Class"] = 0
 			elif g.vector == dataset2[1:]:
@@ -64,8 +72,21 @@ def analyze_range( k_min, k_max, rootdir, dataset1, dataset2 ):
 			writer.writerow(g.kmers)
 					
 		csv_file.flush()
-		
-		# viz.reduce("significant_k_mers.csv")
+	
+	with open("testing_sig_k_mers.csv", 'w', newline="") as csv_file:
+		writer = csv.DictWriter(csv_file, fieldnames=retlist, extrasaction='ignore')
+		writer.writeheader()
+		for g in test_data:
+			if g.vector == dataset1[1:]:
+				g.kmers["Class"] = 0
+			elif g.vector == dataset2[1:]:
+				g.kmers["Class"] = 1
+			
+			writer.writerow(g.kmers)
+					
+		csv_file.flush()
+	
+	# viz.reduce("significant_k_mers.csv")
 
 def main():
 	#k_min, k_max = int(input("Desired k-mer range: ")).split(',')
