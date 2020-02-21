fdc20a9527f1d3475edbb1de4b8524c10defcd34
Anna Levenberg <alevenb1@binghamton.edu>
2020-02-21 17:17:28 -0500

    move jupyter to py script

--- src/cnn/cnn.py
+++ src/cnn/cnn.py
@@ -1,41 +1,96 @@
-import torch 
-import torchvision 
-import torchvision.transforms as transforms 
-# import dask.dataframe as dd
-from keras.utils import to_categorical
+from Bio import SeqIO # FASTA reader
+import numpy as np
 
-TRANSFORM = transforms.Compose([transforms.ToTensor()])
-DATA_PATH = './data'
+def find_min_length(fasta_sequences):
+    seq_lengths = []
 
-# For one-hot encoding
-bmap = {"A":0, "C":1, "G":2, "T":3}
-def one_hot(b):
-    t = [[0,0,0,0]]
-    i = bmap[b]
-    t[0][i] = 1
-    return t
+    for fasta in fasta_sequences:
+        name, sequence = fasta.id, str(fasta.seq)
+        seq_lengths.append(len(sequence))
 
-seq
-# seq_t = Variable(torch.FloatTensor([one_hot(c) for c in seq])).cuda()
+    return min(seq_lengths)
 
-print("Loading CSV...")
-# test = dd.read_csv("../results/training_sig_k_mers.csv", encoding = "UTF-8")
-# train = dd.read_csv("../results/testing_sig_k_mers.csv", encoding = "UTF-8")
+def one_hot_encode(seq):
+    # https://en.wikipedia.org/wiki/FASTA_format#Sequence_representation
+    ltrdict = {'a':[1,0,0,0],'c':[0,1,0,0],'g':[0,0,1,0],'t':[0,0,0,1], 'b':[0,0,0,0], 'v':[0,0,0,0], 'n':[0,0,0,0], 'd':[0,0,0,0], 'm':[0,0,0,0], 'h':[0,0,0,0], 'w':[0,0,0,0], 'y':[0,0,0,0], 's':[0,0,0,0], 'r':[0,0,0,0], 'k':[0,0,0,0]}
+    return [ltrdict[x] for x in seq]
 
-# print("Converting to Tensor...")
-# test_tensor = torch.tensor(test)
-# train_tensor = torch.tensor(train)
-# # # Dataset
-# # train_dataset = torchvision.datasets.MNIST(root=DATA_PATH,
-# #                                            train=True,
-# #                                            transform=TRANSFORM,
-# #                                            download=True)
+def get_data(fasta_sequences):
+    min_len = 9083 # in the dengue_fasta
+    sequences = []
+    for i, fasta in enumerate(fasta_sequences):
+        if i == 300:
+            break
+        name, sequence = fasta.id, str(fasta.seq)
+        one_hot_sequence = one_hot_encode(sequence.lower())
+        if (len(one_hot_sequence) >= min_len):
+            one_hot_sequence = one_hot_sequence[0: (min_len-1)]
+        sequences.append(one_hot_sequence)
+    return np.asarray(sequences)
 
-# # test_dataset = torchvision.datasets.MNIST(root=DATA_PATH,
-# #                                           train=False,
-#                                           transform=TRANSFORM)
+from torch.utils.data import Dataset, DataLoader
+
+class VirLabDataset(Dataset):
+    """ VirLabDataset dataset."""
+
+    # Initialize your data, download, etc.
+    def __init__(self, data, labels):
+        self.len = data.shape[0]
+        self.x_data = data
+        self.y_data = labels
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.len
+
+
+import torch.nn as nn
+import torch.optim as optim
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.mp = nn.MaxPool2d(2)
+        self.fc = nn.Linear(320, 10)
+
+    def forward(self, x):
+        in_size = x.size(0)
+        x = F.relu(self.mp(self.conv1(x)))
+        x = F.relu(self.mp(self.conv2(x)))
+        x = x.view(in_size, -1)  # flatten the tensor
+        x = self.fc(x)
+        return F.log_softmax(x)
 
 def main():
-	# analyze_range(K_MIN, K_MAX, DATASET1, DATASET2)
+    file = "/Users/alevenberg/research/VirLab/src/cnn/genomes/aedes/dengue.fasta"
+    filetype = "fasta"
+    aedes_fasta_sequences = list(SeqIO.parse(file, filetype))
+
+    aedes_data = get_data(aedes_fasta_sequences)
+    aedes_labels = np.ones(300)
+
+    file = "/Users/alevenberg/research/VirLab/src/cnn/genomes/culex/japanese-encephalitis.fasta"
+    filetype = "fasta"
+    culex_fasta_sequences = list(SeqIO.parse(file, filetype))
+
+    min_len = find_min_length(culex_fasta_sequences)
+    culex_data = get_data(culex_fasta_sequences)
+    culex_labels = np.zeros(300)
+
+    data = np.concatenate((aedes_data, culex_data))
+    labels = np.concatenate((aedes_labels, culex_labels))
+
+    dataset = VirLabDataset(data, labels)
+    train_loader = DataLoader(dataset=dataset,
+                              batch_size=30,
+                              shuffle=True,
+                              num_workers=2)
+
+    model = Net()
 
 main()
